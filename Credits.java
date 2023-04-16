a9e6989d2a60b8e7973ca7529fe8251883b40acd
Yuriy Chikhonadskiy <109043213+Alocard8934@users.noreply.github.com>
2023-04-16 20:31:10 +0200

    Add files via upload
    This program shows how to work with strings. Displays a description of the titles in a movie

---
+++ Credits.java
@@ -0,0 +1,25 @@
+package JAVA24;
+
+public class Credits {
+    public static void main(String[] args) {
+        // Movie Information
+        String title = "Shark tornado";
+        int year = 2013;
+        String director = "Antony Ferrante";
+        String role1 = "Fin";
+        String actor1 = "Jen Ziring";
+        String role2 = "April";
+        String actor2 = "Tara Rid";
+        String role3 = "Jorg";
+        String actor3 = "Jon Herd";
+        String role4 = "Nova";
+        String actor4 = "Ceisi Serbo";
+        // information display
+        System.out.println(title + " (" + year + ")\n" + 
+                "Director" + "\t" + actor1 + "\n" +
+                role1 + "\t" + actor1 + "\n" + 
+                role2 + "\t" + actor2 + "\n" + 
+                role3 + "\t" + actor3 + "\n" +
+                role4 + "\t" + actor4);
+    }
+}
